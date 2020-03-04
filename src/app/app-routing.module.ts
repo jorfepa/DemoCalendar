aeb14df6708819f062ed47ad6bd413c9ab730e63
jorfepa <jorfepa@gmail.com>
2020-03-04 15:31:36 -0400

    - Initial structure
    - FullCalendar package added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { ReminderComponent } from './pages/reminder/reminder.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CalendarComponent, data: {tittle: 'Calendar'}},
+  {path: 'reminder/:id', component: ReminderComponent, data: {tittle: 'Reminder'}}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { ReminderComponent } from './pages/reminder/reminder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    ReminderComponent
   ],
   imports: [
     BrowserModule,
